45e7203a3f67ed6bc6f853590b26ca05e8192a3b
Chris Pope <cmpope-tray@chriss-air.lan>
2019-09-24 22:19:30 -0700

    updated files for simple web app

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
-mail = Mail()
 moment = Moment()
-db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -17,12 +13,9 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    mail.init_app(app)
     moment.init_app(app)
-    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
-
